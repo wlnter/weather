b558c009d52c0935bc1f24d38941bad603e2d80a
寒潄 <winter.ld@alibaba-inc.com>
2022-04-12 11:40:14 +0800

    update travis script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,8 @@
 rm -rf build
 npm run build
 
-cd dist
+cd build
 git init
 git add .
 git commit -m 'build and deploy'
-git push --force --quiet "https://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@github.com/wlnter/weather-app.git" master:gh-pages
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/wlnter/weather-app.git" master:gh-pages
