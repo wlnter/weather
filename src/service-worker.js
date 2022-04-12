13db581c7153ab8c2a1556c4208bdd0533004da8
= <winter.ld@alibaba-inc.com>
2022-04-12 21:42:03 +0800

    update

--- src/service-worker.js
+++ src/service-worker.js
@@ -91,3 +91,15 @@ registerRoute(
   },
   new NetworkFirst()
 );
+
+self.addEventListener("install", () => {
+  console.log("service worker 安装成功");
+});
+
+self.addEventListener("activate", () => {
+  console.log("service worker 激活成功");
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("service worker 抓取请求成功: " + event.request.url);
+});
