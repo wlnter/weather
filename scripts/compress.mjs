b9c0fd826c72a5462f4d3587e5e26ac20e86d145
寒潄 <winter.ld@alibaba-inc.com>
2022-04-18 17:40:45 +0800

    update: compress rate

--- scripts/compress.mjs
+++ scripts/compress.mjs
@@ -5,7 +5,7 @@ import imageminWebp from "imagemin-webp";
 (async () => {
   const files = await imagemin(["./src/assets/weatherImageOrigin/*.png"], {
     destination: "./src/assets/weatherImage/",
-    plugins: [imageminWebp({ quality: 80 })],
+    plugins: [imageminWebp({ quality: 50 })],
   });
 
   console.log("Images optimized");
